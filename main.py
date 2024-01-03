a0b540e3ef46c4f400ad532c186de5ea8f464c01
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2024-01-03 03:02:18 +0000

    Commented version of my code in a different website

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ def function():
 
 
 function()
+
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+
+# def hurdle():  
+#   move()
+#   turn_left()
+#   move()
+#   turn_right()
+#   move()
+#   turn_right()
+#   move()
+#   turn_left()
+
+# for hurdles in range (0,6):
+#   hurdle()
